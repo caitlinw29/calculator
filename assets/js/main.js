4461a81a8d02515eaee8b6c7237676f621f5e7b1
Caitlin Wall <caitlinwall96@gmail.com>
2023-09-27 13:12:51 -0400

    Add math.js and respond to user input with basic solving functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,56 @@
 const calculator = document.getElementById('calculator');
 
 calculator.addEventListener('click', (event) => {
-  const isButton = event.target.nodeName === 'BUTTON';
+  //Listen for button clicks only
+  const clickedBtn = event.target;
+  const isButton = clickedBtn.nodeName === 'BUTTON';
   if (!isButton) {
     return;
   }
 
+  //Will need * for multiplying with JS
+  if (clickedBtn.textContent==="x"){
+    clickedBtn.textContent = "*";
+  }
+
+  //Add some spacing to operations
+  const operations = ["+", "*", "-", "/"];
+  for (const operation of operations){
+    if (clickedBtn.textContent.includes(operation)) {
+      clickedBtn.textContent = ` ${operation} `;
+    }
+  }
+
+  //Type what the user clicks, unless it's =
   const typingBlock = document.getElementById('typing-block');
-  typingBlock.innerHTML += event.target.textContent;
+  if (clickedBtn.textContent !== "="){
+    typingBlock.innerHTML += clickedBtn.textContent;
+  }
+
+  //solve when clicking =
+  if(clickedBtn.textContent==="="){
+    calculate(typingBlock.textContent, typingBlock);
+  }
+  
 })
+
+const calculate = (equation, typingBlock) => {
+  //take typed input and make sure numbers are being returned instead of "num"
+  const stringToNumArr = equation.split(',').map(el => {
+    let n = Number(el);
+    return isNaN(n) ? el : n;
+  });
+  //using math.js to eval
+  let answer = math.evaluate(stringToNumArr);
+  typingBlock.innerHTML = answer;
+};
+
+//clear input in the typing block
+// for (const operation of operations){
+//   if (!clickedBtn.textContent.includes(operation)){
+//     clear(typingBlock);
+//   }
+// }
+// const clear = (typingBlock) => {
+//   typingBlock.innerHTML= "";
+// }
